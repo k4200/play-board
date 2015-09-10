db2087c08f202357315f3325ed62e8967f4d4215
yterajima <terra@e2esound.com>
2015-09-11 02:42:32 +0900

    Person クラスのパラメータを定義に合わせ変更

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json._
 
-case class Person(id: Long, name: String, age: Int)
+case class Person(id: Long, email: String, password: String, name: String)
 
 object Person {
   
